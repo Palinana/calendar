031ae80837af1342864ccfbf9d0aaae5129d21fb
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-06-27 15:38:10 -0400

    able to scroll events and made little style changes

--- CalendarFrontEnd/components/EventForm.js
+++ CalendarFrontEnd/components/EventForm.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import history from '../history';
 import { postEvent, deleteEventThunk } from '../store';
 import dateFns from 'date-fns';
+import UpdateEvent from './UpdateEvent';
 
 class EventForm extends Component {
     constructor(props) {
@@ -11,90 +12,105 @@ class EventForm extends Component {
           this.state = {
             startTime: '',
             endTime: '',
-            description: ''
+            description: '',
+            edit: false
           };
           this.handleChange = this.handleChange.bind(this);
           this.handleSubmit = this.handleSubmit.bind(this);
+          this.handleFormShowOnClick = this.handleFormShowOnClick.bind(this);
     }
 
     handleChange(e) {
-      this.setState({[e.target.name]: e.target.value});
-  }
+        this.setState({[e.target.name]: e.target.value});
+    }
 
-  handleSubmit(e) {
+    handleSubmit(e) {
+      let eventStartTime = new Date(this.props.selectedDate);
+      let eventEndTime = new Date(this.props.selectedDate);
+      let startHour = this.parseHour(this.state.startTime);
+      let startMin = this.parseMin(this.state.startTime);
+      let endHour = this.parseHour(this.state.endTime);
+      let endMin = this.parseMin(this.state.endTime);
 
-    let eventStartTime = new Date(this.props.selectedDate);
-    let eventEndTime = new Date(this.props.selectedDate);
-    let startHour = this.parseHour(this.state.startTime);
-    let startMin = this.parseMin(this.state.startTime);
-    let endHour = this.parseHour(this.state.endTime);
-    let endMin = this.parseMin(this.state.endTime);
-
-    eventStartTime.setHours(startHour);
-    eventStartTime.setMinutes(startMin);
-    eventEndTime.setHours(endHour);
-    eventEndTime.setMinutes(endMin);
+      eventStartTime.setHours(startHour);
+      eventStartTime.setMinutes(startMin);
+      eventEndTime.setHours(endHour);
+      eventEndTime.setMinutes(endMin);
 
       e.preventDefault();
-      let body ={
-        startTime: eventStartTime,
-        endTime: eventEndTime,
-        description: this.state.description,
-        date: this.props.selectedDate
+        let body ={
+          startTime: eventStartTime,
+          endTime: eventEndTime,
+          description: this.state.description,
+          date: this.props.selectedDate
       }
 
       this.props.addEvent(body);
-      this.setState({
-          startTime: '', 
-          endTime: '', 
-          description: ''
+        this.setState({
+            startTime: '', 
+            endTime: '', 
+            description: ''
       });
-  }
-  parseHour(timeStr) {
-    let idx = 0;
-    let hour = ''
-    if (timeStr === 0) {
-      return 13
-    } else if (timeStr.indexOf(':') === -1) {
-      return Number(timeStr)
-    } else {
-      idx = timeStr.indexOf(':')
-      hour = timeStr.slice(0,idx)
     }
-    return Number(hour)
-  }
 
-  parseMin(timeStr) {
-    if (timeStr === 0) {
-      return 0
-    } else {
-      let idx = timeStr.indexOf(':')
-      let minutes = timeStr.slice(idx+1)
-      return Number(minutes)
+    parseHour(timeStr) {
+      let idx = 0;
+      let hour = ''
+      if (timeStr === 0) {
+        return 13
+      } else if (timeStr.indexOf(':') === -1) {
+        return Number(timeStr)
+      } else {
+        idx = timeStr.indexOf(':')
+        hour = timeStr.slice(0,idx)
+      }
+      return Number(hour)
     }
-  }
-  renderEvents(events) {
-    let formattedDay = dateFns.format(this.props.selectedDate, 'MM-DD-YYYY');
-      let daysEvents = this.props.allEvents.filter(event => {
-        let formattedEventDay = dateFns.format(event.date, 'MM-DD-YYYY')
-        return formattedDay === formattedEventDay
-      })
 
-      if (!daysEvents.length) {
-        return <div className="no-events">No events for today</div>
-       } else {
-        return daysEvents.map((e,idx) => {
-          let start = dateFns.format(e.startTime, 'h:mm a')
-          let end = dateFns.format(e.endTime, 'h:mm a')
-          return (
-            <div className="events-list" key={idx}>
-              {`${start}-${end}`} <strong> - {e.description}  </strong>
-              <Link to={`/event/delete/${e.id}`} onClick={() => this.props.deleteEvent(e.id)} className="edit-link">delete</Link>
-            </div>
-          )
+    parseMin(timeStr) {
+      if (timeStr === 0) {
+        return 0
+      } else {
+        let idx = timeStr.indexOf(':')
+        let minutes = timeStr.slice(idx+1)
+        return Number(minutes)
+      }
+    }
+
+    handleFormShowOnClick() {
+      this.setState({ edit: true })
+    }
+
+    renderEvents(events) {
+      let formattedDay = dateFns.format(this.props.selectedDate, 'MM-DD-YYYY');
+        let daysEvents = this.props.allEvents.filter(event => {
+          let formattedEventDay = dateFns.format(event.date, 'MM-DD-YYYY')
+          return formattedDay === formattedEventDay
         })
-       }  
-  }
+
+        if (!daysEvents.length) {
+          return <div className="no-events">No events for today</div>
+        } 
+        else {
+          return daysEvents.map((e,idx) => {
+            let start = dateFns.format(e.startTime, 'h:mm a')
+            let end = dateFns.format(e.endTime, 'h:mm a')
+            return (
+              <div className="events-list" key={idx}>
+                {`${start}-${end}`} <strong> - {e.description}  </strong>
+                <Link to={`/event/delete/${e.id}`} onClick={() => this.props.deleteEvent(e.id)} className="delete-link">delete</Link>
+                <button className="edit-btn" onClick={this.handleFormShowOnClick}>edit</button>
+                {
+                  this.state.edit &&
+                  <div className="event-form">
+                      <UpdateEvent hideForm={this.props.hideForm} id={e.id} selectedDate={this.state.selectedDate}/>
+                      </div>
+                    }              
+              </div>
+            )
+          })
+        }  
+    }
 
 
     render() {
@@ -108,11 +124,11 @@ class EventForm extends Component {
               <form onSubmit={this.handleSubmit}>
                   <div>
                       <label>Start Time</label>
-                      <input name="startTime" type="time" onChange={this.handleChange} />
+                      <input name="startTime" defaultValue="12:00" type="time" onChange={this.handleChange} />
                   </div>
                   <div>
                       <label>End Time</label>
-                      <input name="endTime" type="time" onChange={this.handleChange} />
+                      <input name="endTime" defaultValue="12:00" type="time" onChange={this.handleChange} />
                   </div>
                   <div>
                       <label>Description</label>
@@ -128,10 +144,11 @@ class EventForm extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return {
-    allEvents: state.events
-  }
+    return {
+      allEvents: state.events
+    }
 }
+
 const mapDispatchToProps = (dispatch) => {
   return {
     addEvent(body) {
@@ -142,4 +159,5 @@ const mapDispatchToProps = (dispatch) => {
     }
   }
 }
-export default EventForm = connect(mapStateToProps, mapDispatchToProps)(EventForm)
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventForm);
