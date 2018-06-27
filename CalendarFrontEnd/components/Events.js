ac16bb629a5ee910ae8904ceaf81b9097a630bd5
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-06-27 11:19:21 -0400

    can add events

--- CalendarFrontEnd/components/Events.js
+++ CalendarFrontEnd/components/Events.js
@@ -4,26 +4,34 @@ import { getEvents } from '../store';
 
 class Events extends Component {
 
-  renderEvents(events) {
-    return events.map((e,idx) => {
-      let start = dateFns.format(e.startTime, 'h:mm a')
-      let end = dateFns.format(e.endTime, 'h:mm a')
-      return (
-        <div className="event-list" key={idx}>
-          {`${start}-${end}`} <strong>{e.description}</strong>
-        </div>
-      )
+  renderEvents(theDay, events) {
+    var tempArr = [];
+    Object.keys(events).map((e,idx) => {
+        var item = events[e];
+        tempArr.push(item)
+    })
+    let formattedDay = dateFns.format(theDay, 'MM-DD-YYYY')
+      return tempArr.filter(event => {
+        let formattedEvent = dateFns.format(event.date, 'MM-DD-YYYY')
+          return formattedEvent === formattedDay
+      }).map((e,idx) => {
+        let start = dateFns.format(e.startTime, 'h:mm a')
+        let end = dateFns.format(e.endTime, 'h:mm a')
+        return (
+          <div className="event-list" key={idx}>
+            {`${start}-${end}`} <strong>{e.description}</strong>
+          </div>
+         )
     })
   }
 
   render() {
     return (
       <div>
-        {this.renderEvents(this.props.dayEvents)}
+        {this.renderEvents(this.props.day, this.props.dayEvents)}
       </div>
     )
   }
-
 }
 
 export default Events
